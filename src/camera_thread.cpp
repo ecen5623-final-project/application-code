1549370af005f2bda912e4837e16c997fd3076ef
jackn <jackn@zonit.com>
2026-04-18 15:19:56 -0700

    Camera warmup fix

--- src/camera_thread.cpp
+++ src/camera_thread.cpp
@@ -2,6 +2,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <csignal>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/videoio/videoio.hpp>
@@ -11,12 +12,14 @@
 using namespace cv;
 
 extern volatile sig_atomic_t g_stop;
+extern volatile sig_atomic_t g_camera_ready;
 extern CameraBuffer camera_buffer;
 
 #define DEVICE_PATH "/dev/video0"
 #define HRES 640
 #define VRES 480
 #define FPS  30
+#define WARMUP_FRAMES 30
 
 void* camera_thread(void* arg)
 {
@@ -36,6 +39,14 @@ void* camera_thread(void* arg)
     cap.set(CAP_PROP_BUFFERSIZE,   2);
 
     Mat frame;
+
+    // Warmup: capture a few frames to initialize V4L2 buffers
+    printf("Camera warmup...\n");
+    for (int i = 0; i < WARMUP_FRAMES && !g_stop; i++) {
+        cap >> frame;
+    }
+    printf("Camera ready.\n");
+    g_camera_ready = 1;
 
     while (!g_stop) {
         cap >> frame;
