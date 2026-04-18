2f029dc33f57032b8bf327c8c9a08b8621073833
jackn <jackn@zonit.com>
2026-04-18 15:20:02 -0700

    Camera warmup fix p2

--- include/shared.h
+++ include/shared.h
@@ -7,6 +7,7 @@
 #include "display_buffer.h"
 
 extern volatile sig_atomic_t g_stop;
+extern volatile sig_atomic_t g_camera_ready;
 extern CameraBuffer camera_buffer;
 extern DisplayBuffer display_buffer;
 
